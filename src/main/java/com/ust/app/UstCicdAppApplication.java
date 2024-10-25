5fd1ecbc972ff7ff34c07fcb21c0a5dece01d35e
Anup singh <anup.singh@ust.com>
2024-10-25 12:52:55 +0530

    Added the filed

---
+++ src/main/java/com/ust/app/UstCicdAppApplication.java
@@ -0,0 +1,13 @@
+package com.ust.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstCicdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstCicdAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ust/app/api/MyApi.java
@@ -0,0 +1,12 @@
+package com.ust.app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyApi {
+    @GetMapping
+    public String hello() {
+        return "Hello JI, Kaise HO";
+    }
+}
